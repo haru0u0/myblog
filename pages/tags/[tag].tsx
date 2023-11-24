71a8de0a260541c7c84620d00cc2eeb87aa7dfae
haruka <haruka.senzaki@gmail.com>
2023-11-24 13:12:47 +0000

    added missing import

--- pages/tags/[tag].tsx
+++ pages/tags/[tag].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
-import { CMS_NAME } from '../lib/constants'
+import { CMS_NAME } from '../../lib/constants'
 import {getAllTags, getPostsByTag} from "../../lib/api";
 import Post from "../../interfaces/post";
 import Head from "next/head";
@@ -8,6 +8,7 @@ import Header from '../../components/header'
 import Layout from "../../components/layout";
 import Container from "../../components/container";
 import MoreStories from "../../components/more-stories";
+
 
 type Props = {
   posts: Post[],
