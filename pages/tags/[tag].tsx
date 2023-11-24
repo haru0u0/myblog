09d68bd87fbdd7f5bab6398d1a224d5cbcf86b32
haruka <haruka.senzaki@gmail.com>
2023-11-24 13:27:41 +0000

    test

--- pages/tags/[tag].tsx
+++ pages/tags/[tag].tsx
@@ -1,13 +1,13 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import Header from '../components/header'
-import { getAllPosts, getAllTags, getPostsByTag } from '../lib/api'
+import Container from '../../components/container'
+import MoreStories from '../../components/more-stories'
+import Intro from '../../components/intro'
+import Layout from '../../components/layout'
+import Header from '../../components/header'
+import { getAllPosts, getAllTags, getPostsByTag } from '../../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../interfaces/post'
-import Twemoji from '../lib/Twemoji'
+import { CMS_NAME } from '../../lib/constants'
+import Post from '../../interfaces/post'
+import Twemoji from '../../lib/Twemoji'
 
 export default function About() {
   return (
