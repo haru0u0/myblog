c1e82eaea8cc8bc39089e5bce5afa6f4fe1200d4
haruka <haruka.senzaki@gmail.com>
2023-11-25 00:45:46 +0000

    added Link on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
+import Navbar from '../components/navbar'
 import { getAllPosts, getAllTags, getPostsByTag } from '../lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
@@ -22,19 +25,19 @@ export default function Index({ allPosts }: Props) {
         </Head>
         <Container>
           <Intro />
-                <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
+                <h2 className="mb-8 text-5xl md:text-7xl tracking-tighter leading-tight">
         Categories
       </h2>
           <div className="flex flex-wrap  mb-16 gap-4 justify-items-center">
-<a href="/tags/イギリスお仕事" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">イギリスお仕事<Twemoji emoji="🇬🇧" /></a>
-<a href="/tags/イギリス42" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">イギリス42<Twemoji emoji="🇬🇧" /></a>
-<a href="/tags/イギリス大学院留学" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">イギリス大学院留学<Twemoji emoji="🇬🇧" /></a>
+<Link href="/tags/イギリスお仕事" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">イギリスお仕事<Twemoji emoji="🇬🇧" /></a>
+<Link href="/tags/イギリス42" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">イギリス42<Twemoji emoji="🇬🇧" /></a>
+<Link href="/tags/イギリス大学院留学" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">イギリス大学院留学<Twemoji emoji="🇬🇧" /></a>
 <a href="/tags/アメリカ交換留学" className="bg-pink-50 hover:bg-pink-200  text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">アメリカ交換留学 <Twemoji emoji="🇺🇸" /></a>
-<a href="/tags/SWY" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">SWY<Twemoji emoji="🛳 " /></a>
+<Link href="/tags/SWY" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">SWY<Twemoji emoji="🛳 " /></a>
 <a href="/tags/旅行" className="bg-pink-50 hover:bg-pink-200 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">旅行<Twemoji emoji="🇬🇧" /></a>
 <a href="/tags/edtech" className="bg-pink-50 hover:bg-pink-200  text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">edtech<Twemoji emoji="🇺🇸" /></a>
           </div>
-                <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
+                <h2 className="mb-8 text-5xl md:text-7xl tracking-tighter leading-tight">
         Latest Posts
       </h2>
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
