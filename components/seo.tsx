daa71160828d489588f2c8ff3794acb6d46c1c24
haruka <haruka.senzaki@gmail.com>
2024-01-03 15:23:23 +0000

    modified og api

--- components/seo.tsx
+++ components/seo.tsx
@@ -26,7 +26,6 @@ const Seo = ({
     return (
         <Head>
             <title>{title}</title>
-            <meta name="twitter:card" content="summary_large_image" />
             <meta name="viewport" content="width=device-width,initial-scale=1.0" />
             <meta name="description" content={description} />
             <meta property="og:title" content={title} />
@@ -35,6 +34,13 @@ const Seo = ({
             <meta property="og:description" content={description} />
             <meta property="og:type" content="website" />
             <meta property="og:image" content={`https://haru-blog-chi.vercel.app/api/og?title=${title}&emoji=${emoji}&date=${date}`} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="twitter:domain" content="haru-blog-chi.vercel.app" />
+            <meta property="twitter:url" content={`https://blog.haru.ing/${path}`} />
+            <meta name="twitter:title" content="{title}" />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={`https://haru-blog-chi.vercel.app/api/og?title=${title}&emoji=${emoji}&date=${date}`} />
         </Head>
     );
 };
