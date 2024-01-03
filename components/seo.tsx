f8d0b8e5b9bdb7ffb78e582f4553a548e6e2e1d5
haruka <haruka.senzaki@gmail.com>
2024-01-03 02:47:35 +0000

    modified og api

--- components/seo.tsx
+++ components/seo.tsx
@@ -27,7 +27,6 @@ const Seo = ({
         <Head>
             <title>{title}</title>
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:image" content={imgUrl} />
             <meta name="viewport" content="width=device-width,initial-scale=1.0" />
             <meta name="description" content={description} />
             <meta property="og:title" content={title} />
